c5ac5e6b7e98e3171490ae513018e21a904a3225
STUDZPSB\jslusarczyk378 <kuba.slusarczyk.placki@onet.pl>
2021-09-23 17:01:13 +0200

    dd

--- Assets/Scripts/Kratka.cs
+++ Assets/Scripts/Kratka.cs
@@ -2,10 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+enum Wybrane
+{
+    KRZYZYK,
+    KOLKO
+}
+
 public class Kratka : MonoBehaviour
 {
+    public Sprite kolko, krzyzyk;
+
+    Wybrane Wybor = Wybrane.KOLKO;
+    
+
+    SpriteRenderer sr;
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
     private void OnMouseDown()
     {
         Debug.Log("klik klik :)");
+
+        sr.sprite = kolko;
+
+        if (Wybor == Wybrane.KOLKO)
+        {
+            sr.sprite = krzyzyk;
+            Wybrane = Wybrane.KRZYZYK;
+        }
+        else
+        {
+            sr.sprite = kolko;
+        }
     }
 }
