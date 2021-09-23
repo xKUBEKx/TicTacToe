19693a2da9b1b2caebdd5b36571c6abcbcf6c483
STUDZPSB\jslusarczyk378 <kuba.slusarczyk.placki@onet.pl>
2021-09-23 15:27:21 +0200

    :)

---
+++ Assets/Editor/MyWindow.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MyWindow : EditorWindow
+{
+    [MenuItem("Windows/Moje okienko")]
+    public static void OpenWindow()
+    {
+        GetWindow<MyWindow>();
+    }
+}
